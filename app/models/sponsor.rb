acd3843171a60124c9ea794e6b528aad726fb49e
github username <github email address>
2015-04-15 15:23:47 -0400

    added associations to sponsor and student

--- app/models/sponsor.rb
+++ app/models/sponsor.rb
@@ -1,2 +1,5 @@
 class Sponsor < ActiveRecord::Base
+  has_many :needs
+  has_many :student_sponsors
+  has_many :students, through: :student_sponsors
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,6 @@
 class Student < ActiveRecord::Base
+  has_many :skills
+  has_many :needs
+  has_many :student_sponsors
+  has_many :sponsors, through: :student_sponsors
 end
