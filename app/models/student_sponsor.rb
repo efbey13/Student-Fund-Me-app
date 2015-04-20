3c38cb8fa93d40ddcb5381a3b744a81a836ee2be
github username <github email address>
2015-04-20 15:24:20 -0400

    cannot follow self

--- app/models/student_sponsor.rb
+++ app/models/student_sponsor.rb
@@ -4,5 +4,14 @@ class StudentSponsor < ActiveRecord::Base
 
   validates_uniqueness_of :sponsor_id, :scope => [:student_id]
   validates_uniqueness_of :student_id, :scope => [:sponsor_id]
-  
+  validate :cannot_follow_self
+
+  def cannot_follow_self
+    if sponsor_id == student_id
+      errors.add(:student_id, "can't be the same as sponsor id")
+    end
+  end
+
+
+
 end
