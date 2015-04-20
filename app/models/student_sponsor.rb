1c2647755cc47d44a87d0b039ad84ac4b40fa0e8
github username <github email address>
2015-04-20 14:45:18 -0400

    vaidations 01

--- app/models/student_sponsor.rb
+++ app/models/student_sponsor.rb
@@ -1,4 +1,8 @@
 class StudentSponsor < ActiveRecord::Base
   belongs_to :student, :class_name => 'User'
   belongs_to :sponsor, :class_name => 'User'
+
+  validates_uniqueness_of :sponsor_id, :scope => [:student_id]
+  validates_uniqueness_of :student_id, :scope => [:sponsor_id]
+  
 end
