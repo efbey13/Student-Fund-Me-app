36f2722c2283cf1662d0d444b9a4b001b6b6dec6
sebasking01 <noordinarypeopleallowed@gmail.com>
2015-04-21 09:54:37 -0400

    commiting to be able to pull down

--- app/controllers/sponsor_needs_controller.rb
+++ app/controllers/sponsor_needs_controller.rb
@@ -2,6 +2,8 @@ class SponsorNeedsController < ApplicationController
 
   def back_need
     binding.pry
+    @student = User.find(params[:id])
+    StudentSponsor.create(student_id: @student, sponsor_id: current_user.id, sponsored: true)
     SponsorNeed.create(student_need_id: @student_need.id)
     redirect_to student_need_path(@student_need)
   end
