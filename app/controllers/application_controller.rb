35815accd8d7dcfea5bf901716de80b5914eba35
heavenlypeach <mubiynacasey@aol.com>
2015-04-30 17:10:07 -0400

    theme update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :current_user
-  layout :determine_layout
+  # layout :determine_layout
   private
 
   def current_user
@@ -20,16 +20,16 @@ class ApplicationController < ActionController::Base
     session[:user_id] = user.nil? ? nil : user.id
   end
 
-  def determine_layout
-    if current_user == nil
-      'application'
-    else
-         if current_user.status == 'sponsor'
-           "sponsor_profile"
-         elsif current_user.status == 'student'
-           "student_profile"
-         end
-    end
-
-  end
+  # def determine_layout
+  #   if current_user == nil
+  #     'application'
+  #   else
+  #        if current_user.status == 'sponsor'
+  #          "sponsor_profile"
+  #        elsif current_user.status == 'student'
+  #          "student_profile"
+  #        end
+  #   end
+  #
+  # end
 end
